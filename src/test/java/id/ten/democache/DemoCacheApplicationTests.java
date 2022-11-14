2dd4d5c424c91d1e03e01c39fb15801c2053ec58
Teten Nugraha <teten.nugraha18@gmail.com>
2022-11-14 07:29:34 +0700

    feat: prepare project

---
+++ src/test/java/id/ten/democache/DemoCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.democache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
